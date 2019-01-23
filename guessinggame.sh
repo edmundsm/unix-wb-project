dc9180b65a5f28a8709d8576756d2f2655cbee7f
edmundsm <mitchyedmunds@hotmail.co.uk>
2019-01-23 16:50:25 +0000

    Created a function to avoid repeating code

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,29 +2,26 @@
 # the correct amount of files in the current directory
 
 # Function to ask the question since it will be reused
-#function askQuestion {
-#	echo "Can you guess how many files are in the directory?"
-#	read answer
-#}
+function askQuestion {
+	echo "Can you guess how many files are in the directory?"
+	read user_answer
+}
 
 #Initialise the correct answer
 correct_answer=$(ls | wc -l)
 
-echo "Can you guess how many files are in the directory?"
-read user_answer
+askQuestion
 
 while [[ ! $correct_answer -eq $user_answer ]]
 do
 	if [[ $user_answer -gt $correct_answer ]]
 	then
 		echo "Answer too high, try again."
-		echo "Can you guess how many files are in the directory?"
-		read user_answer
+		askQuestion
 	elif [[ $user_answer -lt $correct_answer ]]
 	then
 		echo "Answer too low, try again."
-		echo "Can you guess how many files are in the directory?"
-		read user_answer
+		askQuestion
 	fi
 done
 
